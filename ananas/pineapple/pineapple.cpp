0d1730f591ba209b357e612b42c8953f5c9222e4
NIGGER <alanKYS@KYS.xyz>
2023-10-11 09:44:53 +0200

    ae

--- ananas/pineapple/pineapple.cpp
+++ ananas/pineapple/pineapple.cpp
@@ -5,16 +5,26 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\nae\tarearar\n";
+	std::cout << "mam na imie adrian\n";
+	std::cout << "lubie ananasy\n";
+	std::cout << 'X';//stala znakowa -musi byc jeden znak
+	std::cout << "/n";
+	std::cout << '\n';
+	std::cout << 257 << "\n";
+	std::cout << 12.5 << "\n";
 }
+//skrót do komentarza: ctrl k c komentuje zaznaczone a ctrl k u odkomentuje zaznaczone
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+ //Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+ //Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/* Tips for Getting Started :
+   1. Use the Solution Explorer window to add/manage files
+   2. Use the Team Explorer window to connect to source control
+   3. Use the Output window to see build output and other messages
+   4. Use the Error List window to view errors
+   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+*/
+        
