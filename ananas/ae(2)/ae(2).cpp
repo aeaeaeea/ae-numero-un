3e2d50429951efe7134858741faa6fb226ac73e7
NIGGER <alanKYS@KYS.xyz>
2023-10-25 07:35:15 +0200

    ae

---
+++ ananas/ae(2)/ae(2).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    
+
+}
+
+/*
+algorytm - zbiór instrukcji realizujący postawione przed nim zadanie.
+sposoby zapisu:
+-w punktach
+-opis słowny
+-język programowania lub pseudokod
+-zbiór rysunków
+-schemat blokowy
+*/
