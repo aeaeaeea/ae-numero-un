7c2b7c7ca129f08500ec81c218b16750404214b5
NIGGER <alanKYS@KYS.xyz>
2023-11-14 11:24:54 +0100

    ae

--- ananas/ae(2)/ae(2).cpp
+++ ananas/ae(2)/ae(2).cpp
@@ -49,16 +49,16 @@ void task3()
 
 void task4()
 {
-    float bok_a, bok_b;
+    float sideA, sideB;
 
     std::cout << "\npodaj dlugosc boku a\n";
-    std::cin >> bok_a;
+    std::cin >> sideA;
 
     std::cout << "\npodaj dlugosc boku b\n";
-    std::cin >> bok_b;
+    std::cin >> sideB;
 
-    float area_of_square = bok_a *bok_b;
-    std::cout << "\npole kwadratu wynosi\n" << area_of_square;
+    float areaOfSquare = sideA * sideB;
+    std::cout << "\npole kwadratu wynosi\n" << areaOfSquare;
 }
 
 //Napisz program, który poprosi użytkownika o podanie długości podstawy i wysokości trójkąta, a następnie obliczy pole tego trójkąta, wyświetlając wynik.
@@ -73,8 +73,8 @@ void task5()
     std::cout << "\npodaj wysokosc\n";
     std::cin >> h;
 
-    float area_of_triangle = a * h;
-    std::cout << "\npole trojkata wynosi \n" << area_of_triangle;
+    float areaOfTriangle = a * h;
+    std::cout << "\npole trojkata wynosi \n" << areaOfTriangle;
 }
 
 //Napisz program, który poprosi użytkownika o podanie promienia koła i obliczy pole tego koła, wyświetlając wynik.
@@ -82,16 +82,16 @@ void task5()
 void task6()
 {
     
-    float r;
+    float radius;
 
     std::cout << "\npodaj promien kola\n";
-    std::cin >> r;
+    std::cin >> radius;
 
-    float n = 3.14;
+    float pi = 3.14;
 
-    float area_of_circle = r * n;
+    float areaOfCircle = pi * radius * radius;
     
-    std::cout << "pole kola wynosi " << area_of_circle;
+    std::cout << "pole kola wynosi " << areaOfCircle;
 }
 
 //Napisz program, który poprosi użytkownika o podanie długości podstawy a, podstawy b i wysokości h trapezu, a następnie obliczy pole tego trapezu, wyświetlając wynik.
@@ -99,36 +99,194 @@ void task6()
 void task7()
 {
 
-    float bok_a, bok_b, h;
+    float sideA, sideB, height;
 
     std::cout << "\npodaj bok a\n";
-    std::cin >> bok_a;
+    std::cin >> sideA;
 
     std::cout << "\npodaj bok b\n";
-    std::cin >> bok_b;
+    std::cin >> sideB;
     
     std::cout << "\npodaj wysokosc\n";
-    std::cin >> h;
+    std::cin >> height;
 
-    float area_of_trapeze =  ( bok_a + bok_b ) * h / 2;
+    float areaOfTrapeze =  ( sideA + sideB ) * height / 2;
 
-    std::cout << "pole trapezu wynosi " << area_of_trapeze;
+    std::cout << "pole trapezu wynosi " << areaOfTrapeze;
 }
+
+//Napisz program, który poprosi użytkownika o podanie długości krawędzi sześcianu i obliczy jego objętość, wyświetlając wynik.
 
 void task8()
 {
 
-    float bok_a, 
+    float a;
 
-    std::cout << "\npodaj bok a\n";
-    std::cin >> bok_a;
+    std::cout << "\npodaj krawędź a\n";
+    std::cin >> a;
 
-    float area_of_trapeze = (bok_a + bok_b) * h / 2;
+    float area = ( a * a ) * 6;
 
-    std::cout << "objetosc szesciokata wynosi " << area_of_trapeze;
+    std::cout << "objetosc szesciokata wynosi " << area;
 
 }
 
+//Napisz program, który poprosi użytkownika o podanie temperatury w stopniach Celsiusza, a następnie przeliczy ją na stopnie Fahrenheita i wyświetli wynik.
+
+void task9()
+{
+   
+    float c;
+
+    std::cout << "\npodaj temperature w stopniach celcjusza\n";
+    std::cin >> c;
+
+    float f = c * 2 - 30;
+
+    std::cout << c << "stopni celcjusza to" << f << "stopni fahrenheita\n";
+}
+
+//Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
+
+void task10()
+{
+    float pln;
+    
+    std::cout << "\npodaj ilosć PLN\n";
+    std::cin >> pln;
+
+    float euro = pln / 4.42;
+    float usd = pln / 4.13;
+
+    std::cout << pln << "złoty to " << usd << "usd i " << euro << "euro";
+}
+
+//Napisz program, który poprosi użytkownika o podanie dwóch liczb (a, b) i obliczy średnią arytmetyczną tych liczb. Następnie wyświetli wyni
+
+void task11()
+{
+
+    float a, b;
+
+    std::cout << "\npodaj a\n";
+    std::cin >> a;
+
+    std::cout << "\npodaj b\n";
+    std::cin >> b;
+
+    float avarage = (a + b) / 2;
+
+    std::cout << "\nśrednia wynosi " << avarage;
+}
+
+//Napisz program, który poprosi użytkownika o podanie trzech liczb (a, b, c) i obliczy średnią arytmetyczną tych liczb. Następnie wyświetli wynik.
+
+void task12()
+{
+    float a, b, c, d;
+    
+    std::cout << "\npodaj a\n";
+    std::cin >> a;
+
+    std::cout << "\npodaj b\n";
+    std::cin >> b;
+
+    std::cout << "\npodaj c\n";
+    std::cin >> c; 
+
+    std::cout << "\npodaj d\n";
+    std::cin >> d;
+
+    float avarage = (a + b + c + d) / 4;
+
+    std::cout << "\nśrednia arytmetyczna wynosi " << avarage;
+
+}
+
+//Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+
+void task13()
+{
+    float w1, w2, w3,a ,b ,c;
+
+    std::cout << "\npodaj liczbę a \n";
+    std::cin >> a;
+
+    std::cout << "\npodaj liczbę b \n";
+    std::cin >> b;
+
+    std::cout << "\npodaj liczbę c \n";
+    std::cin >> c;
+
+    std::cout << "\npodaj wage liczby a \n";
+    std::cin >> w1;
+
+    std::cout << "\npodaj wage liczby b \n";
+    std::cin >> w2;
+
+    std::cout << "\npodaj wage liczby c \n";
+    std::cin >> w3;
+
+    float sumOfWages = w1 + w2 + w3;
+    float avarage = (a * w1 + b * w2 + c * w3) / sumOfWages;
+
+    std::cout << "\n średnia wynosi " << avarage;
+}
+
+//Napisz program, który poprosi użytkownika o podanie dwóch liczb całkowitych. Następnie wykona operacje (+, -, *, /) i wyświetli wyniki.
+
+void task14()
+{
+    long long a, b;
+
+    std::cout << "\npodaj a\n";
+    std::cin >> a;
+
+    std::cout << "\npodaj b\n";
+    std::cin >> b;
+
+    long long adding = a + b;
+    long long dividing = a / b;
+    long long multiplication = a * b;
+    long long subtraction = a - b;
+
+    std::cout << "\nwynik dodawania to " << adding << "\nwynik odejmowania to " << subtraction << "\nwynik mnożenia " << multiplication << "\nwynik dzielenia to " << dividing;
+}
+
+//Napisz program, który poprosi użytkownika o podanie liczby sekund i przeliczy je na godziny i minuty, wyświetlając wynik.
+
+void task15()
+{
+    float s;
+
+    std::cout << "\npodaj sekundy \n";
+    std::cin >> s;
+
+    float min = s / 60;
+    float h = s / 60 / 60;
+
+    std::cout << "\nminuty " << min << "\ngodziny " << h;
+}
+
+//Napisz program, który poprosi użytkownika o podanie współrzędnych (x1, y1) i (x2, y2) dwóch punktów w układzie kartezjańskim, a następnie obliczy odległość między nimi, wyświetlając wynik.
+
+void task16()
+{
+    float x1, x2, y1, y2;
+
+    std::cout << "\npodaj x1 \n";
+    std::cin >> x1;
+    std::cout << "\npodaj x2 \n";
+    std::cin >> x2;
+    std::cout << "\npodaj y1 \n";
+    std::cin >> y1;
+    std::cout << "\npodaj y2 \n";
+    std::cin >> y2;
+
+    float distance = (x1 - x2) * (x1 - x2) - (y1 - y2) * (y1 - y2);
+
+    std::cout << "\nodleglosc wynosi " << distance;
+}
 int main()
 {
 
@@ -145,6 +303,24 @@ int main()
     task6();
 
     task7();
+
+    task8();
+
+    task9();
+
+    task10();
+
+    task11();
+
+    task12();
+
+    task13();
+
+    task14();
+
+    task15();
+
+    task16();
 }
 
 /*
