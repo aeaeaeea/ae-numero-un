6abf7b0de9f83930451a4e92ba98b962870a81da
NIGGER <alanKYS@KYS.xyz>
2023-12-06 09:49:31 +0100

    ae

---
+++ ananas/InstructionInConsoleApplication/InstructionInConsoleApplication.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+/*
+operator porównania:
+> wiekszosci
+< mniejszosci 
+>= wieksze lub rowne
+<= mniejsze lub rowne
+== rowne
+!= rozne
+*/
+
+//napisz program ktory pobierze liczbe od uzytkownika i wyswietli czy jest ona dodatnia czy nie
+//wersja 1
+void task1()
+{
+	int number;
+
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+
+	if (number > 0)
+	{
+		std::cout << "liczba jest dodatnia\n";
+	}
+	if (number <= 0)
+	{
+		std::cout << "liczba jest ujemna\n";
+	}
+}
+
+//wersja 2
+void task2()
+{
+	int number;
+
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+
+	if (number > 0)
+	{
+		std::cout << "liczba jest dodatnia\n";
+	}
+	else
+	{
+		std::cout << "liczba jest ujemna\n";
+	}
+}
+
+//wersja 3
+void task3()
+{
+	int number;
+
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+
+	if (number > 0)
+	{
+		std::cout << "liczba jest dodatnia\n";
+	}
+	else if (number < 0)
+	{
+		std::cout << "liczba jest ujemna\n";
+	}
+	else
+	{
+		std::cout << "liczba jest równa zeru\n";
+	}
+}
+
+int main()
+{
+	task1();
+}
