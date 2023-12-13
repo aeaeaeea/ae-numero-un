e29f36e6640014011c018ff7b0d0adacc9594b86
NIGGER <alanKYS@KYS.xyz>
2023-12-13 09:43:35 +0100

    dfgdfgdfg

--- ananas/InstructionInConsoleApplication/InstructionInConsoleApplication.cpp
+++ ananas/InstructionInConsoleApplication/InstructionInConsoleApplication.cpp
@@ -1,6 +1,16 @@
 ﻿#include <iostream>
 
 /*
+priorytety operatorow:
+
+* /
++ -
+> < >= <= == !=
+&&
+||
+
+
+
 operator porównania:
 > wiekszosci
 < mniejszosci 
@@ -8,6 +18,19 @@ operator porównania:
 <= mniejsze lub rowne
 == rowne
 != rozne
+
+operatory logiczne:
+
+|| or/lub
+&& and\lub
+! not/nie
+
+A B		A || B		A && B
+f f       f			  f
+t f       t			  f
+f t       t		      f
+t t       t			  t
+
 */
 
 //napisz program ktory pobierze liczbe od uzytkownika i wyswietli czy jest ona dodatnia czy nie
@@ -69,7 +92,55 @@ void task3()
 	}
 }
 
+//.....................................................
+//wersja 1
+void task4()
+{
+	int number;
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+
+	if (number >= 1)
+
+	{
+		if (number < 10)
+		{
+			std::cout << "liczba jest w zakresie\n";
+		}
+		else
+		{
+			std::cout << "liczba nie jest w zakresie\n";
+		}
+	}
+
+	else
+	{
+		std::cout << "liczba nie jest w zsakresie\n";
+	}
+}
+//wersja 2 
+void task5()
+{
+
+	int number;
+
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+
+	if (number >= 1 && number < 10)
+	{
+		std::cout << "liczba nalezy do zbioru\n";
+	}
+	else
+	{
+		std::cout << "liczba nie nalezy do zbioru\n";
+	}
+}
+
+
 int main()
 {
-	task1();
+	//task1();
+	task5
+	();
 }
