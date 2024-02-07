d12436545d37b04e813e1a538d194050a245571e
NIGGER <alanKYS@KYS.xyz>
2024-02-07 10:31:41 +0100

    df

--- ananas/doWhile/doWhile.cpp
+++ ananas/doWhile/doWhile.cpp
@@ -1,4 +1,5 @@
-﻿//napisz program ktory wczyta od uzytkownika liczbe dodarina a
+﻿#include <iostream>
+//napisz program ktory wczyta od uzytkownika liczbe dodarina a
 
 void task1()
 {
@@ -64,11 +65,97 @@ void task2()
 	std::cout << "podano liczbe " << number << "\n";
 }
 
-#include <iostream>
+//napisz program ktory wylosuje liczbe a  uzsytkownik bedzie musial ja zgadnac
+void task3()
+{
+	int randomNumber;
+
+	srand(time(0));
+	randomNumber = rand() % 101;
+	
+	int numberFromUser;
+
+	/*if (numberFromUser != randomNumber)
+	{
+		std::cout << "podaj liczbe";
+		std::cin >> numberFromUser;
+		if (numberFromUser != randomNumber)
+		{
+			std::cout << "podaj liczbe";
+			std::cin >> numberFromUser;
+			if (numberFromUser != randomNumber)
+			{
+				std::cout << "podaj liczbe";
+				std::cin >> numberFromUser;
+				if (numberFromUser != randomNumber)
+				{
+					std::cout << "podaj liczbe";
+					std::cin >> numberFromUser;
+					if (numberFromUser != randomNumber)
+					{
+						std::cout << "podaj liczbe";
+						std::cin >> numberFromUser;
+						if (numberFromUser != randomNumber)
+						{
+							std::cout << "podaj liczbe";
+							std::cin >> numberFromUser;
+							//...
+						}
+					}
+				}
+			}
+		}
+	}*/
+
+	do
+	{
+		std::cout << "podaj liczbe\n";
+		std::cin >> numberFromUser;
+		if (numberFromUser < randomNumber)
+		{
+			std::cout << "za mala liczba\n";
+		}
+		if (numberFromUser > randomNumber)
+		{
+			std::cout << "za duza liczba\n";
+		}
+	} while (numberFromUser != randomNumber);
+
+	//std::cout << "Liczba losowa" << randomNumber << "\n";
+}
+
+//napisz program wyswietlajacy liczby calkowite z przedzialu <0,x) (wartosc x podaje uzytkownik)
+void task4()
+{
+	int upperRange, tempNumber = 0;
+	std::cout << "podaj gorny zakres\n";
+	std::cin >> upperRange;
+
+	/*std::cout << "0, ";
+	if (upperRange >= 1)
+	{
+		std::cout << "1, ";
+		if (upperRange >= 2)
+		{
+			std::cout << "2, ";
+			if (upperRange >= 3)
+			{
+				std::cout << "3, ";
+				//...
+			}
+		}
+	}*/
+	do
+	{
+		std::cout << tempNumber << ", ";
+		tempNumber =  tempNumber + 1;
+		//tempNumber += 1, tempNumber ++
+	} while (upperRange >= tempNumber);
+}
 
 int main()
 {
-	task2;
+	task4();
 }
 
 
