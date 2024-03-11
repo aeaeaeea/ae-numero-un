59e40a747fe478a795ce31e07672a99f9bb61797
pineapple <adrian.mietelski@wp.pl>
2024-03-11 22:35:26 +0100

    ae

--- ananas/for/for.cpp
+++ ananas/for/for.cpp
@@ -5,9 +5,9 @@
 * 
 * 
 * 
-* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
-* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
-* Program obliczający sumę kwadratów liczb od 1 do 10
+* .
+* 
+* 
 * Program, który wczyta podstawę oraz wykładnik oraz wyliczy wynik.
 * Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
 
@@ -83,9 +83,26 @@ void task4()
 //Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
 void task5()
 {
-	int numberFromUser;
-	std::cout << "podaj liczbe dodatnia";
+	int numberFromUser, temp = 0;
+	std::cout << "podaj liczbe dodatnia\n";
 	std::cin >> numberFromUser;
+	
+	for (int i = 1; i < numberFromUser; i++)
+	{
+		if (numberFromUser % i == 0)
+		{
+			temp = temp + i;
+		}
+	}
+
+	if (temp == numberFromUser)
+	{
+		std::cout << "liczb jest liczba doskonala";
+	}
+	else
+	{
+		std::cout << "liczb nie jest liczba doskonala";
+	}
 }
 
 //Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
@@ -117,10 +134,58 @@ void task7()
 //Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
 void task8()
 {
+	int factoria = 1;
 
+	for (int i = 1; i <= 10; i++)
+	{
+		factoria = i * factoria;
+	}
+
+	std::cout << factoria;
+}
+
+//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.)
+void task9()
+{
+	int temp = 0;
+
+	for (int i = 1; i < 10; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			temp = i * j;
+
+			std::cout << temp << "\n";
+		}
+	}
+}
+
+//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+void task10()
+{
+	for (int i = 1; i < 101; i++)
+	{
+		if(i % 3 == 0)
+		{
+			std::cout << i << "\n";
+		}
+	}
+}
+
+//Program obliczający sumę kwadratów liczb od 1 do 10
+void task11()
+{
+	int temp = 0;
+
+	for (int i = 1; i < 11; i++)
+	{
+		temp = i * i + temp;
+	}
+	
+	std::cout << temp;
 }
 
 int main()
 {
-	task7();
+	task11();
 }
