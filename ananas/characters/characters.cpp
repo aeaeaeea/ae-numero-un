71ba38258d000614c86073305b301060ec806e51
NIGGER <alanKYS@KYS.xyz>
2024-03-22 15:01:03 +0100

    ae

---
+++ ananas/characters/characters.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//napisz program ktory pobierze znak od uzytkownika i pobierze go.
+void task1()
+{
+	char characterFromUser;
+	cout << "napisz pojedynczy znak\n";
+	cin >> characterFromUser;
+
+	cout << characterFromUser;
+
+	characterFromUser = 97;
+	characterFromUser = 'a';
+	characterFromUser = 'a' + 1;
+}
+
+int main()
+{
+	task1();;
+
+}
