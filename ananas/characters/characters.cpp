02a14be6e0c3ec294bf875a72067002575ce4df8
NIGGER <alanKYS@KYS.xyz>
2024-04-03 10:29:39 +0200

    ae

--- ananas/characters/characters.cpp
+++ ananas/characters/characters.cpp
@@ -16,8 +16,62 @@ void task1()
 	characterFromUser = 'a' + 1;
 }
 
-int main()
+//napisz program ktory sprawdzi czy to mala litera alfabetu czy duza
+void task2()
 {
-	task1();;
+	char characterFromUser;
+	cout << "podaj znak\n";
+	cin >> characterFromUser;
 
+	//v1
+	if (characterFromUser <= 'z' && characterFromUser >= 'a')
+	{
+		cout << "podales mala litere alfabetu";
+	}
+	else
+	{
+		cout << "podales inny znak";
+	}
+
+	//v2
+	if (characterFromUser <= 122 && characterFromUser >= 97)
+	{
+		cout << "podales mala litere alfabetu";
+	}
+	else
+	{
+		cout << "podales inny znak";
+	}
+}
+
+//napisz program który poprosi cię o twoje imię i przywita
+void task3()
+{
+	string nameFromUser;
+	cout << "podaj imie\n";
+	cin >> nameFromUser;
+
+	cout << "witaj " << nameFromUser;
+}
+
+//program ktory sprawdzi czy haslo jest poprawne
+void task4()
+{
+	string passwordFromUser;
+	cout << "podaj haslo\n";
+	cin >> passwordFromUser;
+
+	if (passwordFromUser == "pineapple")
+	{
+		cout << "haslo poprawne";
+	}
+	else
+	{
+		cout << "haslo niepoprawne";
+	}
+}
+
+int main()
+ {
+	task4();
 }
